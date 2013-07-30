6678b8a670d7a34cd5836053ee667aec2a851e2b
Orf <tom@tomforb.es>
2013-07-30 16:22:58 +0100

    Add package info

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='django-choice-object',
     version='0.5',
     packages=['django_choice_object'],
-    url='',
+    url='https://github.com/orf/django-choice-object',
     license='',
     author='Tom',
-    author_email='',
-    description=''
+    author_email='tom@tomforb.es',
+    description='A choice object for Django forms and models'
 )
