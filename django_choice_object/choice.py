1f1ccb3bfd5ab2d752d0e78d9a567112217712c3
Michael K <michael-k@users.noreply.github.com>
2015-10-15 23:50:39 +0200

    Early continue
    This should make it easier to understand the if conditions

--- django_choice_object/choice.py
+++ django_choice_object/choice.py
@@ -8,15 +8,18 @@ class ChoiceMetaclass(type):
         cls._order_key = 0 if (getattr(cls, "_order_by", "value") == "value") else 1
 
         for name, value in inspect.getmembers(cls):
-            if not name.startswith("_") and \
-                    not (inspect.isfunction(value) or inspect.ismethod(value) or type(value) is classmethod):
-                if isinstance(value, tuple) and len(value) > 1:
-                    value, display_name = value[0], value[1]
-                    setattr(cls, name, value)
-                else:
-                    display_name = " ".join(x.capitalize() for x in name.split("_"))
+            if name.startswith("_"):
+                continue
+            if inspect.isfunction(value) or inspect.ismethod(value) or type(value) is classmethod:
+                continue
 
-                cls._data[value] = display_name
+            if isinstance(value, tuple) and len(value) > 1:
+                value, display_name = value[0], value[1]
+                setattr(cls, name, value)
+            else:
+                display_name = " ".join(x.capitalize() for x in name.split("_"))
+
+            cls._data[value] = display_name
 
         # So we need to access the ._data attribute of any parent classes so we can access the
         if hasattr(cls.__base__, "_data"):
